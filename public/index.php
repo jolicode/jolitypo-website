ec7ffb1a275b0e725f31523da331b8d391319bd8
Damien ALEXANDRE <mobman02@gmail.com>
2020-12-09 19:12:46 +0100

    Fix the Generated PHP Code

--- public/index.php
+++ public/index.php
@@ -30,5 +30,10 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
-$response->send();
+$response->send(<<<DDD
+
+DDD
+);
 $kernel->terminate($request, $response);
+
+
